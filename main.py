cf850423391b51b635d63b3d2d4b9fb46fb0a0d6
gleyce <gleycesouza@poli.ufrj.br>
2022-02-22 14:50:42 -0300

    debug main.py

--- main.py
+++ main.py
@@ -7,38 +7,40 @@ import pandas as pd
 
 try:
     option = webdriver.ChromeOptions()
-    brave_path = "C:/Program Files (x86)/BraveSoftware/Brave-Browser/Application/brave.exe"
+    brave_path = r"C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\brave.exe"
     option.binary_location = brave_path
-    driver = webdriver.Chrome(chrome_options=option, executable_path="G:/My Drive/Interno/Projetos/Projeto Busca Capes/chromedriver.exe")
+    driver = webdriver.Chrome(chrome_options=option, executable_path= r"G:\My Drive\Interno\Projetos\country-costs\chromedriver.exe")
     
-    # countries = ['brazil','bolivia', 'china', 'egypt']
-    countries = ['brazil']
+    countries = ['brazil','bolivia', 'china', 'egypt']
+    # countries = ['brazil']
     result = []
 
+    page = f'https://www.lonelyplanet.com/'
+    driver.get(page)
+    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//*[text()='Accept Cookies']"))).click()
+    print('cookies accepted')
+
     for country in countries:
+        try:    
+            print(f'checking costs for {country}...')
+            page = f'https://www.lonelyplanet.com/{country}/essential-information'
+            driver.get(page)
 
-        print(f'checking costs for {country}...')
-        page = f'https://www.lonelyplanet.com/{country}/essential-information'
-
-        driver.get(page)
-
-        try:
-            WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//*[text()='Accept Cookies']"))).click()
-
-        finally:
-            WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//*[text()='Money and costs']"))).click()
-
+            print('trying access page money and costs...')
+            WebDriverWait(driver, 80).until(EC.presence_of_element_located((By.XPATH, "//*[text()='Money and costs']"))).click()
+            
             currency = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*/text()[.="Currency"]/following::div[@class="jsx-3059088443 dangerHTML leading-relaxed lg:text-lg"]')))
             string_daily_costs = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*/text()[.="Daily Costs"]/following::div[@class="jsx-3059088443 dangerHTML leading-relaxed lg:text-lg"]')))
-            daily_costs = string_daily_costs.text.split('\n')
-            daily_costs = [y.split(": ") for y in daily_costs]
+
+            list_daily_costs = string_daily_costs.text.split('\n')
+
+            daily_costs = [y.split(": ") for y in list_daily_costs]
+
             list_costs = [{'name':item[0],'value':item[1]} for item in daily_costs]
 
-            result.append({
-                    'country': country,
-                    'currency': currency.text,
-                    'costs': list_costs[0]
-                })
+            result.append({'country': country,
+                            'currency': currency.text,
+                            'costs': list_costs})
 
             final_df = pd.DataFrame()
 
@@ -47,15 +49,10 @@ try:
                 df['country'] = item['country']
                 df['currency'] = item['currency']
                 final_df = pd.concat([final_df, df])
-                # final_df.merge(df, how='inner', on='country')
 
-            # # df.to_csv('costs_country.csv')
-
-            # # df = pd.DataFrame(result)
-            # print(final_df.head())
-
-    print(result)
-    input()
+            final_df.to_csv('costs_country.csv')
+        except:
+            print(f'error {country}')
 
 except Exception as e:
     print(e)
